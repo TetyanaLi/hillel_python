89f83a400e5869b49cabe589e2f5be506b2016b7
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-02 18:47:06 +0200

    lesson7-3 updated

--- LESSON_1/LESSON_7/lesson7-3.py
+++ LESSON_1/LESSON_7/lesson7-3.py
@@ -23,18 +23,28 @@ import datetime
 import requests
 URL = 'http://api.openweathermap.org/data/2.5/forecast/daily'
 
+#user_input_city = input('Enter city: ') # С ГОРОДОМ не ПОЛУЧАЕТСЯ пока
+user_input_count = int(input('Enter count: '))
+
 def return_weather(days=1):
     data = {'q': 'Odesa', 'cnt': days, 'units': 'metric','appid': 'f9ada9efec6a3934dad5f30068fdcbb8'}
     r = requests.get(URL, data)
     return r.json()
-result = return_weather(days=5)
+result = return_weather (user_input_count)
 
 print('Дата\t\tTемпература днем\tПо ощущениям днем\tТемпература ночью\tПо ощущениям ночью')
 for day in result['list']:
     data = day['dt']
-    print(datetime.datetime.fromtimestamp(data).strftime("%d-%m-%Y"), '\t'*2, day['temp']['day'],
-          '\t' * 4, day['feels_like']['day'], '\t'*5, day['temp']['night'], '\t'*4, day['feels_like']['night'])
+    print(str(datetime.datetime.fromtimestamp(data).strftime("%d-%m-%Y")), '\t'*2, str(day['temp']['day']),
+          '\t' * 4, str(day['feels_like']['day']), '\t'*5, str(day['temp']['night']), '\t'*4,
+          str(day['feels_like']['night']))
 
-def write_file(result):
-    with open('dv.txt','w') as f:
-        f.writelines([day])
+name_of_file = str(datetime.datetime.today().strftime("%d-%m-%Y")) + '-Odessa-' + str(user_input_count) + '-days-forecast.txt'
+with open(name_of_file ,'w') as f:
+    f.writelines('Дата\t\tTемпература днем\tПо ощущениям днем\tТемпература ночью\tПо ощущениям ночью'+'\n')
+    for day in result['list']:
+        data = day['dt']
+        f.writelines(str(datetime.datetime.fromtimestamp(data).strftime("%d-%m-%Y"))+'\t' * 2 + str(day['temp']['day'])
+                          + '\t' * 4 + str(day['feels_like']['day']) + '\t' * 5 + str(day['temp']['night'])
+                          + '\t' * 4 + str(day['feels_like']['night']) + '\n')
+
