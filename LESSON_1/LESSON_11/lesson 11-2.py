088dfca424ea10e7f35f6189a6c030c4ef12a8be
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-08 00:27:54 +0200

    lesson11-3 3 варианта

--- LESSON_1/LESSON_11/lesson 11-2.py
+++ LESSON_1/LESSON_11/lesson 11-2.py
@@ -1,7 +1,14 @@
 #2. Написать функцию которая будет частично скрывать e-mail, пример:
 #hide_email(somebody_email@gmail.com) -> em***@**il.com
 
-any_text = somebody_email@gmail.com
+some_list = str('somebody_email@gmail.com')
 
-
-def hide_email(any_list):
+def hide_email (some_list):
+    elements = some_list.split('@')
+    print
+    new_list = []
+    for i in elements:
+        new_list.append(len(i))
+        d = dict(zip(new_list, elements))
+        lw = d.get(max(new_list))
+    return lw
--- LESSON_1/LESSON_11/lesson 11-3.py
+++ LESSON_1/LESSON_11/lesson 11-3.py
@@ -2,6 +2,42 @@
 # longest_word("What makes a good man") -> makes
 #x = input('Введите действительное число с двумя знаками после десятичной точки: ')
 
+some_list = str('What makes a good man')
+def longest_word(some_list):
+    elements = some_list.split()
+    new_list = []
+    for i in elements:
+        new_list.append(len(i))
+        d = dict(zip(new_list, elements))
+        lw = d.get(max(new_list))
+    return lw
+print(longest_word(some_list)) # 1 ВАРИАНТ
+
+def longest_word(some_list):
+    elements = {len(val): val for val in some_list.split()}
+    return elements[max(elements)]
+some_list = str('What makes a good man')
+print(longest_word(some_list))  # 2 ВАРИАНТ
+
+def longest_word(some_string):
+    return max(some_string.split(), key=len)
+some_list = str('What makes a good man')
+print(longest_word(some_list))  # 3 ВАРИАНТ
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #some_list = str('What makes a good man')
 #elements = some_list.split()
  #def longest_word(elements[0:]):
@@ -17,16 +53,6 @@
 #print(d)
 #print(d.get(max(new_list)))
 
-some_list = str('What makes a good man')
-def longest_word(some_list):
-    elements = some_list.split()
-    new_list = []
-    for i in elements:
-        new_list.append(len(i))
-        d = dict(zip(new_list, elements))
-        lw = d.get(max(new_list))
-    return lw
-print(longest_word(some_list))
 
 
 
