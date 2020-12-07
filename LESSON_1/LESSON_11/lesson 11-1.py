8aa001f5a9acfa6fe07023f979623fdfd82ec162
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-07 19:19:35 +0200

    lesson11-1

--- LESSON_1/LESSON_11/lesson 11-1.py
+++ LESSON_1/LESSON_11/lesson 11-1.py
@@ -4,3 +4,9 @@
 # Пример : count_points(3, 2, 2) -> 11
 
 win = int(input())
+draw = int(input())
+loss = int(input())
+def count_points(win, draw, loss):
+    summ_coint = (win * 3) + (draw * 1) + (loss * 0)  # loss не влияет на результат
+    return summ_coint
+print(count_points(win, draw, loss))
--- LESSON_1/LESSON_11/lesson 11-2.py
+++ LESSON_1/LESSON_11/lesson 11-2.py
@@ -1,2 +1,7 @@
 #2. Написать функцию которая будет частично скрывать e-mail, пример:
 #hide_email(somebody_email@gmail.com) -> em***@**il.com
+
+any_text = somebody_email@gmail.com
+
+
+def hide_email(any_list):
