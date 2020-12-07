f8bfc16b3f383db132e88341ad72b2f1fe9a85cc
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-08 00:47:59 +0200

    lesson11-2

--- LESSON_1/LESSON_11/lesson 11-2.py
+++ LESSON_1/LESSON_11/lesson 11-2.py
@@ -1,14 +1,10 @@
 #2. Написать функцию которая будет частично скрывать e-mail, пример:
 #hide_email(somebody_email@gmail.com) -> em***@**il.com
 
-some_list = str('somebody_email@gmail.com')
 
-def hide_email (some_list):
-    elements = some_list.split('@')
-    print
-    new_list = []
-    for i in elements:
-        new_list.append(len(i))
-        d = dict(zip(new_list, elements))
-        lw = d.get(max(new_list))
-    return lw
+
+def hide_email (email):
+    email = '@'.join([email.replace(email[email.find('@') - 3:], '***'),
+                      email.replace(email[:len(email) - email.find('@') + 2], '**')])
+    return email
+print(hide_email('somebody_email@gmail.com'))
