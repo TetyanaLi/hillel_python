cdb19bc6f6af387ca3eabdbc86f3efb771739608
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-08 00:55:07 +0200

    RENAME DIRECT

--- LEVEL_1/LESSON_11/lesson 11-4.py
+++ LEVEL_1/LESSON_11/lesson 11-4.py
@@ -4,3 +4,6 @@
 #  1) -> 'Marvel makes good movies, DC makes good comics'
 #  fake_string('DC makes good movies, DC makes good comics', 'DC', 'Marvel',
 #  2) -> 'Marvel makes good movies, Marvel makes good comics'
+
+
+# ДОЛЖОК
