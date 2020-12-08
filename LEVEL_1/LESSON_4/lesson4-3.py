64f7d299c61104e445627c2370c3a5891f306066
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-08 02:36:23 +0200

    lesson4-3.py

--- LEVEL_1/LESSON_4/lesson4-3.py
+++ LEVEL_1/LESSON_4/lesson4-3.py
@@ -1,2 +1,14 @@
 #3. Даны два целых числа `A` и `В`. Выведите все числа от `A` до `B` включительно,
 # в порядке возрастания, если `A < B`, или в порядке убывания в противном случае.
+
+number_1 = int(input('введите целое число - '))
+number_2 = int(input('введите целое число - '))
+if number_1 < number_2:
+    numbers = list(range(number_1, number_2 + 1))
+    print(numbers)
+elif number_2 < number_1:
+    numbers = list(range(number_1, number_2 - 1, -1))
+    print(numbers)
+else:
+    print('Ошибка ввода!!!')
+
