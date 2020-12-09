58f4e2ef6ebc2377f98d082d456d087164143e81
TetyanaLi <litvinovatetyana@gmail.com>
2020-12-09 23:18:59 +0200

    lesson 12-1

--- LEVEL_1/LESSON_12/lesson12-1.py
+++ LEVEL_1/LESSON_12/lesson12-1.py
@@ -11,20 +11,39 @@
 #   При изменении баланса записывать в транзакции (сумма, тип операции, текущая_дата)
 #   * доп. добавить и учитывать банковские комиссии (1%)
 
-class Bank_account():
+from datetime import datetime
+import uuid
+
+class BankAccount:
+
+
     """Банковский счет."""
-    def __init__(self, name,id, balance, transaction):
-        """Инициализирует атрибуты str, uuid, float, list."""
+    def __init__(self, name):
+        """Инициализирует атрибуты."""
         self.name = name
-        self.id = id
-        self.balance = balance
-        self.transactions = transaction
-    def deposit_of_funds(self):
+        self.account_id = uuid.uuid4()
+        self.balance = 0.0
+        self.transactions = []
+
+    def deposit_of_funds(self, amount):
         """Депозит средств."""
-        print(f"{self.} is now deposit_of_funds.")
-    def withdrawal_of_funds(self):
+        self.balance += amount - (amount * 0.01)
+        self.transactions.append((amount, 'deposit', datetime.now()))
+
+    def withdrawal_of_funds(self, amount):
         """Вывод средств."""
-        print(f"{self.transactions} is now withdrawal_of_funds")
+        self.balance -= amount + (amount * 0.01)
+        self.transactions.append((amount, 'withdrawal', datetime.now()))
+
     def get_balance(self):
         """Получить баланс."""
-        print(f"{self.balance} is now balance")
+        return self.balance
+
+first_client = BankAccount('First_client')
+first_client.deposit_of_funds(200)
+print(first_client.get_balance())
+first_client.withdrawal_of_funds(100)
+print(first_client.get_balance())
+first_client.withdrawal_of_funds(300)
+print(first_client.get_balance())
+print(first_client.transactions)
